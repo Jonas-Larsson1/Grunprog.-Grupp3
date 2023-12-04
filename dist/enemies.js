88bb8c60667cd73516b543a9d3ce54e13de1c788
Jonas Larsson <jonas_larsson1@outlook.com>
2023-12-04 17:39:28 +0100

    enemies can follow path

--- dist/enemies.js
+++ dist/enemies.js
@@ -11,22 +11,26 @@ export const enemySpawnTimer = (game) => {
     game.enemySpawnTimer -= game.deltaTime
     if (game.enemySpawnTimer <= 0) {
         spawnEnemy(game)
-        game.enemySpawnTimer = Math.random() * 2 + 1
+        game.enemySpawnTimer = Math.random() * 2 + 2
     }
 }
 
 export const updateEnemies = (game) => {
+    const margin = game.tileSize / 2
+
     game.enemies.forEach(enemy => {
         const target = game.path[enemy.pathIndex]
-        if (enemy.x < target.x) {
-            enemy.x += enemy.vel * game.deltaTime
-        } else if (enemy.x > target.x) {
-            enemy.x -= enemy.vel * game.deltaTime
-        } else if (enemy.y < target.y) {
-            enemy.y += enemy.vel * game.deltaTime
-        } else if (enemy.y > target.y) {
-            enemy.y -= enemy.vel * game.deltaTime
-        } else if (enemy.x === target.x && enemy.y === target.y) {
+        const speed = enemy.vel * game.deltaTime
+
+        if (enemy.x < target.x) enemy.x += speed
+        else if (enemy.x > target.x) enemy.x -= speed
+        if (enemy.y < target.y) enemy.y += speed
+        else if (enemy.y > target.y) enemy.y -= speed
+
+        const distanceX = Math.abs(enemy.x + (enemy.size / 2) - target.x)
+        const distanceY = Math.abs(enemy.y + (enemy.size / 2) - target.y)
+
+        if (distanceX <= margin && distanceY <= margin) {
             enemy.pathIndex++
         }
     })
@@ -40,6 +44,8 @@ export const drawEnemies = (ctx, game) => {
 }
 
 export const spawnEnemy = (game) => {
+    console.log(game.enemies[0])
+
     const enemy = {
         x: (game.startTile.x * game.tileSize) + (game.tileSize / 4),
         y: (game.startTile.y * game.tileSize) + (game.tileSize / 4),
