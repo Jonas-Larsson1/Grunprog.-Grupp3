83b026d5dee23b47d492ad2e505ce0cec8b60f39
djaevil <djaevil1@gmail.com>
2023-12-18 20:32:46 +0100

    börjat kod för waves

---
+++ dist/scripts/waves.js
@@ -0,0 +1,27 @@
+let wavesButton = document.getElementById("wavesButton")
+let wavesContainer = document.getElementById("wavesContainer")
+
+wavesButton.addEventListener("click", () => {
+    wavesText()
+})
+
+const wavesText = () => {
+    
+
+}
+ 
+
+
+
+
+/*
+export const wavesMessage = (game) => {
+    if(game.timer < 30) {
+        
+    } else if (game.timer > 30 && game.timer < 60) {
+        //2nd wave
+    } else if (game.timer > 60) {
+        //Final wave
+    }
+}
+*/
